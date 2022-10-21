b2b47978cebac6eeec02a5d5b37c9cc6bc3cca12
macsimtv <maximelecouturier@gmail.com>
2022-10-21 16:32:13 +0200

    [Fix] Api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://api.mapsos.app-web.ml',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
